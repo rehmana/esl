882d5298909a87c8fa50bd0d6a549c4e91c28b7d
Attique Rehman <a.rehman@justsearch.com>
2013-07-31 15:52:24 +0100

    First Image

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Abdali.org</h1>
+    <div class="shape" id="shape1"></div>
+    <div class="shape" id="shape2"></div>
+	<div class="shape" id="shape3"></div>
+	<div class="shape" id="shape4"></div> 	 
+   	
+</body>
+</html>
